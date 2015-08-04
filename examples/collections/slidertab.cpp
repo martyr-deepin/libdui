7036c3d85dce6c0dd2339aaf545f29375b45c266
ysw <yinshaowen241@gmail.com>
2015-08-04 17:49:23 +0800

    optimize the dslider style

--- examples/collections/slidertab.cpp
+++ examples/collections/slidertab.cpp
@@ -1,7 +1,12 @@
+#include "dslider.h"
 #include "slidertab.h"
+
+DUI_USE_NAMESPACE
 
 SliderTab::SliderTab(QWidget *parent) : QWidget(parent)
 {
-
+    DSlider * slider = new DSlider(Qt::Horizontal, this);
+    slider->setFixedWidth(300);
+    slider->move(5, 100);
 }
 
